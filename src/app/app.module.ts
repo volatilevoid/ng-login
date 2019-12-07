696ddfd4fa8e217ec4fc1e704ff85a3671bd70fb
Slavko Petrovic <slavko@axelyos.com>
2019-12-07 17:40:30 +0100

    Add Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mock-allowed-users.ts
@@ -0,0 +1,8 @@
+import { User } from './models/user';
+
+export const ALLOWEDUSERS: User[] = [
+    {
+        email: 'user@mail.com',
+        password: 'pass123'
+    }
+];
